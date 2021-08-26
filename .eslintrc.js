a52d67697fa922c4131b35946c122d4ec887c516
Diego Arcega <diego@quiqup.com>
2021-08-26 15:03:24 -0300

    feat: added custom eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:optimize-regex/recommended',
+    'plugin:array-func/all',
+    'plugin:json/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    es6: true,
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div className="flex">
       <h1>diego</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
